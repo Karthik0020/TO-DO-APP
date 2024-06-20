f1ee84d9fd3684779e89aef5ab12154d530efead
karthik0020 <karthikkumar0020@gmail.com>
2024-06-20 22:21:36 +0530

    init

--- to-do-app/apps/user/app/page.tsx
+++ to-do-app/apps/user/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div>
-      jiijijijiiii
-    </div>
-  );
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  if(session?.user) {
+    redirect('/user')
+  } else{
+    redirect('/api/auth/signin')
+  }
+
 }
